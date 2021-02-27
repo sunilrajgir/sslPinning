0c339e6fe0261dcec09e9c7a0c006345a4ccf50e
Sunil Kumar29 <sunil.kumar29@olacabs.com>
2021-02-27 21:12:29 +0530

    added alert

--- SSL/SSL/ViewController.swift
+++ SSL/SSL/ViewController.swift
@@ -8,12 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        guard let url = URL(string: "https://www.google.co.uk") else { return }
+        ServiceManager().callAPI(withURL: url, isCertificatePinning: true) { (message) in
+            let alert = UIAlertController(title: "SSLPinning", message: message, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
     }
-
-
 }
 
